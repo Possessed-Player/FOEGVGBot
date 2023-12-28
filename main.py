d96d8e295aedcb6834bd8a98397ee1d727343715
Possessed Player <127137555+Possessed-Player@users.noreply.github.com>
2023-12-28 16:03:02 +0300

    Added prompt for lost units, Fixed freeze issues
    Now bot checks for lost units, with prompt at the start to choose if you rather not do that(to save time/better performance).
    
    Issue where the bot freezes after the second round of attack is now fixed.

--- main.py
+++ main.py
@@ -29,11 +29,13 @@ def collect_dialog_data():
 	aabtn_pos = (helper.last_x + 40, helper.last_y + 15)
 	aabtn_clr = helper.get_color(aabtn_pos[0], aabtn_pos[1])
 	slot_pos = (helper.last_x + 50, helper.last_y - 290)
-	helper.click(slot_pos[0], slot_pos[1])
+	if ARLU: helper.click(slot_pos[0], slot_pos[1])
 	sleep(0.5)
 	slot_clr = helper.get_color(slot_pos[0], slot_pos[1])
 	tab_pos = (helper.last_x - 90, helper.last_y - 205)
-	helper.click(tab_pos[0] + 20, tab_pos[1] + 70)
+	if ARLU: helper.click(tab_pos[0] + (45 * (TAB - 1)), tab_pos[1])
+	sleep(0.25)
+	if ARLU: helper.click(tab_pos[0] + 20, tab_pos[1] + 70)
 	sleep(0.25)
 	return slot_pos, slot_clr, tab_pos, aabtn_pos, aabtn_clr
 	
@@ -48,6 +50,11 @@ def collect_okbtn():
 	return okbtn_pos, helper.get_color(okbtn_pos[0], okbtn_pos[1])  
 	
 def main():
+	if FAST:
+		CLICK = helper.fast_click
+	else:
+		CLICK = helper.click
+		
 	while True:
 		if helper.on:
 			sect_pos = helper.mouse_position()
@@ -69,37 +76,34 @@ def main():
 			while helper.get_color(aabtn_pos[0], aabtn_pos[1]) != aabtn_clr:
 				if not helper.on: break
 				if not clicked:
-					helper.fast_click(sect_pos[0], sect_pos[1])
+					CLICK(sect_pos[0], sect_pos[1])
 					if not NPC:
 						sleep(0.05)
-						helper.fast_click(men_pos[0], men_pos[1])
+						CLICK(men_pos[0], men_pos[1])
 					clicked = True
 			
-			if helper.get_color(slot_pos[0], slot_pos[1]) == slot_clr:
-				helper.fast_click(tab_pos[0] + (45 * (TAB - 1)), tab_pos[1])
-				if SLEEP: sleep(SLEEP[0])
-				helper.fast_click(tab_pos[0] + 20, tab_pos[1] + 70)
+			# Replace unit if lost(color is brown == empty slot == lost unit)
+			if ARLU and (helper.get_color(slot_pos[0], slot_pos[1]) == slot_clr):
+				CLICK(tab_pos[0] + (45 * (TAB - 1)), tab_pos[1])
+				CLICK(tab_pos[0] + 20, tab_pos[1] + 70)
 				while helper.get_color(slot_pos[0], slot_pos[1]) == slot_clr:
 					if not helper.on: break
-					helper.fast_click(tab_pos[0] + 20, tab_pos[1] + 70)
-					if SLEEP: sleep(SLEEP[0])
+					CLICK(tab_pos[0] + 20, tab_pos[1] + 70)
 				
 			
-			helper.fast_click(aabtn_pos[0], aabtn_pos[1])
+			CLICK(aabtn_pos[0], aabtn_pos[1])
 			
 			while helper.get_color(okbtn_pos[0], okbtn_pos[1]) != okbtn_clr:
 				if not helper.on: break
-				if SLEEP: SLEEP[0]
 				
-			helper.fast_click(okbtn_pos[0], okbtn_pos[1])
-			if SLEEP: sleep(SLEEP[1])
+			CLICK(okbtn_pos[0], okbtn_pos[1])
 
 
 if __name__ == "__main__":
-	global helper, SLEEP, TAB, NPC
+	global helper, FAST, TAB, NPC, ARLU
 	helper = bt.Assistant()
 	helper.threshold = DEFAULT_THRESH
-	helper.on_off_htkey("e")
+	helper.on_off_htkey("q")
 	
 	while True:
 		ans = re.match("(1|2)", input("\n -- Choose one of the attacking options -- \n" + \
@@ -107,10 +111,7 @@ if __name__ == "__main__":
 		
 		if ans:
 			ans = ans.group(1)
-			if ans == "1":
-				NPC = False
-			else:
-				NPC = True
+			NPC = (ans == "2")
 				
 			break
 			
@@ -119,13 +120,20 @@ if __name__ == "__main__":
 									"1. Fast		2. Very Fast \n"))
 		if ans:
 			ans = ans.group(1)
-			if ans == "1":
-				SLEEP = (0.0005, 0.001) 
-			else:
-				SLEEP = None
+			FAST = (ans == "2")
 				
 			break
 			
+	while True:
+		ans = re.match("(1|2)", input("\n -- Do you want the bot to replace lost units? -- \n" + \
+										"1. Yes		2. No \n"))
+		
+		if ans:
+			ans = ans.group(1)
+			ARLU = (ans == "1")
+				
+			break
+	
 	while True:
 		ans = re.match("([1-6])", input("\n -- Type in the tab number to use troops from -- \n"))
 		
